c7a535b1ec2e21290c4ae7f8866c7f70336dd4ab
Dennis Regnersen <deofrivia@gmail.com>
2021-11-27 02:05:33 +0300

    1.0 version
    required functions are done

--- Prog-Lab-5/main.c
+++ Prog-Lab-5/main.c
@@ -1,35 +1,99 @@
 #include <stdio.h>
+#include <string.h>
 #include "bmpmaster.h"
 #include "conway.h"
 
-int main() {
-    FILE *in = fopen("C:\\Users\\deofr\\C\\Prog-Lab-5\\mickey.bmp", "rb");
+#define LINESIZE 200
 
-    if (in == NULL) {
-        printf("ERROR! File not found.\n");
+int parseInt(char *number) {
+    int result = 0;
+    int length = strlen(number);
+    int power = 1;
+
+    for (int i = length - 1; i >= 0; i--) {
+        result += (number[i] - '0') * power;
+        power *= 10;
     }
 
-    FileHeader file_h = readFileHeader(in);
+    return result;
+}
 
-    InfoHeader info_h = readInfoHeader(in);
+void setFileName(char *dest, char *directory, char *initial, int number) {
+    char new_filename[LINESIZE];
+    char global_name[LINESIZE];
 
-    Pixel **array = parsePixelArray(in, file_h, info_h);
+    sscanf(strrchr(initial, '\\'), "\\%[^.]", global_name);
+    sprintf(new_filename, "%s\\new_%s_%d.bmp", directory, global_name, number);
 
-    ft_iterateLife(&array, info_h.Height, info_h.Width, 5);
+    strcpy(dest, new_filename);
+}
 
-    for (int i = 0; i < info_h.Height; i++) {
-        for (int j = 0; j < info_h.Width; j++) {
-            if (array[i][j] == 0) {
-                printf("X");
-            } else {
-                printf(" ");
-            }
+int main(int argc, char **argv) {
+    char *filename, *directory;
+    int iter_count = 0, max_iter = 0;
+    int frequency = 1;
+    short isSrc = 0, isDir = 0, isMax = 0;
+
+    for (int i = 1; i < argc; i++) {
+        char *command = argv[i];
+        if (!strcmp(command, "--input")) {
+            filename = argv[++i];
+            isSrc = 1;
+        } else if (!strcmp(command, "--output")) {
+            directory = argv[++i];
+            isDir = 1;
+        } else if (!strcmp(command, "--max_iter")) {
+            max_iter = parseInt(argv[++i]);
+            isMax = 1;
+        } else if (!strcmp(command, "--dump_freq")) {
+            frequency = parseInt(argv[++i]);
+        } else {
+            printf("Command '%s' is unacceptable\n", command);
         }
-        printf("\n");
     }
 
-    createFile("C:\\Users\\deofr\\C\\Prog-Lab-5\\new-file.bmp", file_h, info_h, array);
+    if (isSrc && isDir) {
+        FILE *in = fopen(filename, "rb");
 
-    array = NULL;
+        if (in == NULL) {
+            printf("File does not exist.\n");
+        }
+
+        FileHeader file_h = readFileHeader(in);
+
+        InfoHeader info_h = readInfoHeader(in);
+
+        Pixel **pixel_arr = parsePixelArray(in, file_h, info_h);
+
+        fclose(in);
+
+        char isIterated[LINESIZE];
+
+        printf("Print 'stop' to quit.\n");
+        printf("ITERATE:");
+        gets(isIterated);
+
+        while (strcmp(isIterated, "stop") != 0 && (!isMax || iter_count < max_iter)) {
+            for (int i = 0; i < frequency; i++) {
+                iterateLife(&pixel_arr, info_h.Height, info_h.Width);
+
+                char new_filename[LINESIZE];
+                setFileName(new_filename, directory, filename, ++iter_count);
+
+                createFile(new_filename, file_h, info_h, pixel_arr);
+            }
+            printf("ITERATE:");
+            gets(isIterated);
+        }
+
+        pixel_arr = NULL;
+    } else {
+        if (!isSrc) {
+            printf("Source file is absent.\n");
+        }
+        if (!isDir) {
+            printf("Directory is absent.\n");
+        }
+    }
     return 0;
 }
